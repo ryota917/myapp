a50f5b7de872da27da6ed375f75f0fc420db3f72
sasataniryota <61574281+sasataniryota@users.noreply.github.com>
2020-05-06 18:11:53 +0900

    fix

--- config/puma.rb
+++ config/puma.rb
@@ -36,7 +36,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-workers Integer(ENV['WEB_CONCURRENCY'])
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
